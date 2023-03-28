aef655f7abdb61477695e841ce52fafdca64c9a7
Izadora Netz <izadora.ns@gmail.com>
2023-03-28 19:45:17 -0300

    utilizando biblioteca bulma para estilizar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -7,9 +8,26 @@ function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
